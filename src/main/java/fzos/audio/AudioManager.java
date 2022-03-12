8437d2c0c36c36ddbb79ab3cd12f00eab89b7b77
fhh200000 <1041154549@qq.com>
2022-03-13 00:13:04 +0800

    Bug Fix:WAV play.

--- src/main/java/fzos/audio/AudioManager.java
+++ src/main/java/fzos/audio/AudioManager.java
@@ -9,18 +9,6 @@ import java.lang.reflect.Field;
 
 @FzOSInternalImplementation
 public class AudioManager {
-    /*
-    * typedef struct {
-    U8 id[4];
-    U32 size;
-    U16 audio_format;
-    U16 num_channels;
-    U32 sample_rate;
-    U32 byte_rate;
-    U16 block_align;
-    U16 bits_per_sample;
-}__attribute__((packed)) WavFormatChunk;
-    */
     public static Audio openAudioFromFile(File f) throws Exception {
         Field stringField =  File.class.getDeclaredField("filepath");
         stringField.setAccessible(true);
@@ -31,15 +19,14 @@ public class AudioManager {
         return new Audio((int) audioFormat.getSampleRate(), audioFormat.getChannels(), audioFormat.getSampleSizeInBits(), data);
     }
     public static void playAudio(Audio a) throws Exception {
-        final AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,a.sampleRate,a.sampleDepth, a.channels, 2,2,false);
+        final AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,a.sampleRate,a.sampleDepth, a.channels, a.channels*a.sampleDepth/8,a.sampleRate,false);
         AudioInputStream audioStream = new AudioInputStream(new ByteArrayInputStream(a.data),format,a.data.length);
-        System.out.printf("%d %d %d %d\n",a.data.length,a.sampleRate,a.sampleDepth,a.channels);
-        System.out.println(audioStream.available());
         Clip c = AudioSystem.getClip();
         c.addLineListener(event -> {
             if (event.getType() == LineEvent.Type.STOP) System.out.println("Done");
         });
         c.open(audioStream);
         c.start();
+        c.drain();
     }
 }
