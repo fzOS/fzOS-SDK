b331aeaba07cdbd3666ebac23c95f3e0f2d73d6f
fhh200000 <1041154549@qq.com>
2022-03-30 21:32:10 +0800

    Bug Fix: Audio declaration.

--- src/main/java/fzos/audio/Audio.java
+++ src/main/java/fzos/audio/Audio.java
@@ -4,6 +4,8 @@ public class Audio {
     public int sampleRate;
     public int channels;
     public int sampleDepth;
+    private int dataLength; //INTERNAL USE ONLY, UNUSABLE BY SDK!
+    private int offsetToPcm;  //INTERNAL USE ONLY, UNUSABLE BY SDK!
     public byte[] data;
     protected Audio(int sampleRate, int channels, int sampleDepth, byte[] data) {
         this.sampleRate = sampleRate;
